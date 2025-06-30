b9f177dd46eeb2908b57a5cd109e82ca4b2fc4da
Amrit Aujla <amritpalaujla204@gmail.com>
2025-06-30 13:39:03 -0700

    images for crop selection panel

---
+++ vite-project/src/assets/cropList.js
@@ -0,0 +1,9 @@
+import cherries from "../assets/cherries.jpg";
+import grapes from "./grapes.jpg";
+import peaches from "./peaches.jpg";
+
+export const cropList = [
+  { name: "Cherries", image: cherries },
+  { name: "Grapes", image: grapes },
+  { name: "Peaches", image: peaches },
+];
