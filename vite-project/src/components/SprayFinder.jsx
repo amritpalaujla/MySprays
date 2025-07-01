9c4798cfffc5b88c56cb7dc720154e209ea36140
Amrit Aujla <amritpalaujla204@gmail.com>
2025-06-30 17:07:48 -0700

    displaying cards on panel

--- vite-project/src/components/SprayFinder.jsx
+++ vite-project/src/components/SprayFinder.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import { cropList } from "../assets/cropList";
-
+import "./SprayFinder.css";
 function SprayFinder() {
   const [selectedCrop, setSelectedCrop] = useState(null);
 
   return (
     <div>
       {cropList.map((crop) => {
-        return <div key={crop.name}>{crop.name}</div>;
+        return (
+          <div
+            key={crop.name}
+            onClick={() => {
+              console.log("you just clicked", crop.name);
+              setSelectedCrop(crop.name);
+            }}
+            className="crop-card"
+          >
+            <img src={crop.image} alt={crop.name}></img>
+            <p>{crop.name}</p>
+          </div>
+        );
       })}
     </div>
   );
