abbdb3cc72f4aacc1befcc65995d19e3b0d67513
Amrit Aujla <amritpalaujla204@gmail.com>
2025-07-13 15:26:10 -0700

    added some more fruit

--- vite-project/src/assets/cropList.js
+++ vite-project/src/assets/cropList.js
@@ -1,9 +1,15 @@
 import cherries from "../assets/cherries.jpg";
 import grapes from "./grapes.jpg";
 import peaches from "./peaches.jpg";
+import apricots from "./apricots.jpg";
+import plums from "./plums.jpg";
+import nectarines from "./nectarines.jpg";
 
 export const cropList = [
   { name: "Cherries", image: cherries },
   { name: "Grapes", image: grapes },
   { name: "Peaches", image: peaches },
+  { name: "Apricots", image: apricots },
+  { name: "Plums", image: plums },
+  { name: "Nectarines", image: nectarines },
 ];
