60eedcfe872af92b695f72f992208365b211a045
Amrit Aujla <amritpalaujla204@gmail.com>
2025-07-15 15:52:27 -0700

    state for selectedissue

--- vite-project/src/components/IssueDropDown.jsx
+++ vite-project/src/components/IssueDropDown.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import sprayData from "../assets/sprayData.json";
 function IssueDropDown({ crop }) {
   const issues = Object.keys(sprayData[crop]); // this extracts the issues and puts them in an array
+  const [selectedIssue, setSelectedIssue] = useState("");
   return (
     <div>
       <h3>select issue for {crop}</h3>
       {console.log("sprayData crop 1", sprayData[crop])}
-      <select>
+      <select
+        placeholder="select an issue"
+        value={selectedIssue}
+        onChange={(e) => setSelectedIssue(e.target.value)}
+      >
+        <option value="" disabled>
+          -- Select an issue --
+        </option>
         {issues.map((issue) => (
           <option key={issue} value={issue}>
             {issue}
