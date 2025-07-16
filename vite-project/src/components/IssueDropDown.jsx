18f1569c9626175bc6fcdf0e82e12528bb6f5f47
Amrit Aujla <amritpalaujla204@gmail.com>
2025-07-16 11:31:23 -0700

    showing spray based on issue

--- vite-project/src/components/IssueDropDown.jsx
+++ vite-project/src/components/IssueDropDown.jsx
@@ -3,6 +3,8 @@ import sprayData from "../assets/sprayData.json";
 function IssueDropDown({ crop }) {
   const issues = Object.keys(sprayData[crop]); // this extracts the issues and puts them in an array
   const [selectedIssue, setSelectedIssue] = useState("");
+
+  const sprays = sprayData[crop][selectedIssue];
   return (
     <div>
       <h3>select issue for {crop}</h3>
@@ -21,6 +23,14 @@ function IssueDropDown({ crop }) {
           </option>
         ))}
       </select>
+
+      {selectedIssue &&
+        sprays.map((spray) => (
+          <div key={spray.name}>
+            <h4>{spray.name}</h4>
+            <p>{spray.rate}</p>
+          </div>
+        ))}
     </div>
   );
 }
