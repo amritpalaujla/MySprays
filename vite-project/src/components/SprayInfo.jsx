dba3f22c51dbfa1e0e83ea91e4a57b28ad9a2fee
Amrit Aujla <amritpalaujla204@gmail.com>
2025-08-31 18:41:18 -0700

    update

--- vite-project/src/components/SprayInfo.jsx
+++ vite-project/src/components/SprayInfo.jsx
@@ -68,7 +68,7 @@ function SprayInfo({ token }) {
       {/* Modal */}
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-black/30 backdrop-blur-sm bg-opacity-40 z-50">
-          <div className="bg-white rounded-lg shadow-lg p-6 w-full max-w-md relative">
+          <div className="m-3 bg-white rounded-lg shadow-lg p-6 w-full max-w-md relative">
             <button
               onClick={() => setIsModalOpen(false)}
               className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
