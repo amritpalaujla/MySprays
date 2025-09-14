1b6b0092b22f7cb6bd63a7a4bc211616b8b5b604
Amrit Aujla <amritpalaujla204@gmail.com>
2025-09-14 14:36:09 -0700

    trying to fix date issue

--- vite-project/src/components/SprayInfo.jsx
+++ vite-project/src/components/SprayInfo.jsx
@@ -178,6 +178,7 @@ function SprayInfo({ user }) {
     setEditingSprayId(spray._id);
     const formatDateForInput = (dateString) => {
       const date = new Date(dateString);
+      date.setDate(date.getDate() + 1);
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, "0");
       const day = String(date.getDate()).padStart(2, "0");
