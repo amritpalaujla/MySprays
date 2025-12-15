e6147e715d3f3961705675fa8750c492a95044e4
Amrit Aujla <amritpalaujla204@gmail.com>
2025-12-15 10:28:40 -0800

    cors origin to netlify domain

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 3000; // the port where our server will run
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["https://mysprays.netlify.app"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
